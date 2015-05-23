de62eb05330ea71a3755e2b6a15753f1b391091a
Jens <nens90@gmail.com>
2015-05-23 17:42:06 +0200

    ..

--- src/parameter.py
+++ src/parameter.py
@@ -1,21 +1,20 @@
 #!/usr/bin/env python
 
-takeoff_alt = 1
+takeoff_alt = 0.7
 size_of_square = 0.5
-threshold = 0.3
+threshold = 0.15
 sandbox_buffer = 0.3
 
-sandbox = [2, 2, 2]
+sandbox = [1, 1.5, 2]
 safezone = [sandbox[0]-sandbox_buffer, sandbox[1]-sandbox_buffer, sandbox[2]-sandbox_buffer]
 
 takeoff = [[0, 0, takeoff_alt]]
 
-landing = [[0, 0, takeoff_alt*0.6],
-           [0, 0, takeoff_alt*0.4],
-           [0, 0, takeoff_alt*0.2],
-           [0, 0, 0]]
+landing = [[0, 0, 0.1]]
 
-square = [[0, size_of_square, takeoff_alt],
-          [size_of_square, 0, takeoff_alt],
-          [0, -size_of_square, takeoff_alt], 
-          [-size_of_square, 0, takeoff_alt]]
+# square = [[0, size_of_square, takeoff_alt],
+#           [size_of_square, 0, takeoff_alt],
+#           [0, -size_of_square, takeoff_alt], 
+#           [-size_of_square, 0, takeoff_alt]]
+
+square = [[0,size_of_square, takeoff_alt]]
